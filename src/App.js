eba18941d1f479767770c7396cefb798fde9f968
Ntutu Sekonyela <n.sekonyela@alustudent.com>
2023-09-17 21:04:46 +0200

    created interesting proj

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+        <p>
+          React services
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
